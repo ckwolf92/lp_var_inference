e5f45ea3d3b68041fbd74c35455f883e79db7f4e
mikkelpm <mikkel.p.m@gmail.com>
2024-06-07 10:12:38 -0400

    + worst-case coverage of VAR Wald ellipsoid

--- analytics/figures_worstcase.m
+++ analytics/figures_worstcase.m
@@ -1,6 +1,6 @@
 clear all;
 
-% Worst-case coverage of conventional VAR CI,
+% Worst-case coverage of conventional VAR CI/ellipsoid,
 % comparison of bias-aware VAR CI to LP CI,
 % and optimal bias-aware CI
 
@@ -8,9 +8,10 @@ clear all;
 %% Settings
 
 signif = 0.1;                           % Significance level
-se_ratio = linspace(0.001,0.999,100);   % Values of SE ratio to plot (sqrt{aVar(AR)/aVar(LP)})
+se_ratio = sort([linspace(0.001,0.999,100) 0.25 0.5]); % Values of SE ratio to plot (sqrt{aVar(AR)/aVar(LP)})
+k = [2 5 10];                           % Values of k to plot (for joint Wald confidence ellipsoid)
 M = [0.1 1 1.5 2 3];                    % Values of M to plot
-linestyles = {'-','--','-.',':','.'};   % Line style for each value of M
+linestyles = {'-k','--k','-.k',':k','.k'}; % Line style for each value of M
 plot_pos = [0 0 8 4];                   % Figure position (in inches)
 plot_ylim_length = [0 2];               % Vertical axis limits for relative length plot
 
@@ -22,11 +23,11 @@ n_r = length(se_ratio);
 
 z = norminv(1-signif/2); % Normal critical value
 r = @(b,c) normcdf(b-c)+normcdf(-b-c); % Rejection probability
-cv = @(b) fzero(@(c) r(b,c)-signif,z+b); % Bias-aware critical value
+cv = @(b) fzero(@(c) r(b,c)-signif,z+b); % Bias-aware critical value (note: this is faster than ncx2inv command)
 prob_fct = @(k,b) log(r(b,z))+log(1-r(b/k,z)); % Log joint probability Hausman rejects and VAR CI doesn't cover
 
-% Worst-case coverage of conventional CI as function of relative length
-wc_cov = 1-r(M(:).*sqrt(1./(se_ratio(:).^2)'-1),z);
+noncentr = M(:).^2.*(se_ratio(:).^(-2)'-1); % Worst-case noncentrality parameter
+wc_cov = 1-r(sqrt(noncentr),z); % Worst-case coverage of conventional CI as function of relative length
 
 cv_var_aware = nan(n_M,n_r);
 weight_opt_aware = nan(n_M,n_r);
@@ -42,7 +43,7 @@ for i_r=1:n_r
     for i_M=1:n_M
 
         % Critical value for bias-aware VAR CI
-        cv_var_aware(i_M,i_r) = cv(M(i_M)*the_aux);
+        cv_var_aware(i_M,i_r) = cv(sqrt(noncentr(i_M,i_r)));
 
         % Optimal bias-aware CI
         the_obj = @(w) cv((1-w)*M(i_M)*the_aux/sqrt(1+w^2*the_aux^2))*sqrt(1+w^2*the_aux^2);
@@ -63,6 +64,13 @@ end
 
 length_var_aware = (cv_var_aware/z).*se_ratio; % Relative length of bias-aware VAR CI
 
+% Worst-case coverage of joint Wald confidence ellipsoid
+n_k = length(k);
+wc_prob_wald = nan(n_M,n_r,n_k);
+for i_k=1:n_k
+    wc_prob_wald(:,:,i_k) = ncx2cdf(chi2inv(1-signif,k(i_k)),k(i_k),noncentr);
+end
+
 
 %% Plot
 
@@ -70,46 +78,50 @@ length_var_aware = (cv_var_aware/z).*se_ratio; % Relative length of bias-aware V
 figure('Units','inches','Position',plot_pos);
 hold on;
 for i_M=1:n_M
-    plot(se_ratio,wc_cov(i_M,:),linestyles{i_M},'Color','k','LineWidth',1);
+    plot(se_ratio,wc_cov(i_M,:),linestyles{i_M},'LineWidth',1);
 end
 hold off;
 the_xlim = xlim;
 line(the_xlim,[1 1]*(1-signif),'Color','k','LineStyle','-'); % Mark nominal coverage level on vertical axis
 xlim(the_xlim);
 ylim([0 1]);
+label_axis();
 label_legend(M);
 
 % Relative length of bias-aware VAR CI
 figure('Units','inches','Position',plot_pos);
 hold on;
 for i_M=1:n_M
-    plot(se_ratio,length_var_aware(i_M,:),linestyles{i_M},'Color','k','LineWidth',1);
+    plot(se_ratio,length_var_aware(i_M,:),linestyles{i_M},'LineWidth',1);
 end
 hold off;
 the_xlim = xlim;
 line(the_xlim,[1 1],'Color','k','LineStyle','-'); % Mark 1 on vertical axis
 xlim(the_xlim);
 ylim(plot_ylim_length);
+label_axis();
 label_legend(M);
 
 % Weight on LP in optimal bias-aware CI
 figure('Units','inches','Position',plot_pos);
 hold on;
 for i_M=1:n_M
-    plot(se_ratio,weight_opt_aware(i_M,:),linestyles{i_M},'Color','k','LineWidth',1);
+    plot(se_ratio,weight_opt_aware(i_M,:),linestyles{i_M},'LineWidth',1);
 end
 hold off;
 ylim([0 1]);
+label_axis();
 label_legend(M);
 
 % Relative length of optimal bias-aware CI
 figure('Units','inches','Position',plot_pos);
 hold on;
 for i_M=1:n_M
-    plot(se_ratio,length_opt_aware(i_M,:),linestyles{i_M},'Color','k','LineWidth',1);
+    plot(se_ratio,length_opt_aware(i_M,:),linestyles{i_M},'LineWidth',1);
 end
 hold off;
 ylim([0 1]);
+label_axis();
 label_legend(M);
 
 % Worst-case joint probability
@@ -119,15 +131,37 @@ the_xlim = xlim;
 line(the_xlim,[1 1]*signif,'Color','k','LineStyle',':'); % Mark nominal significance level on vertical axis
 xlim(the_xlim);
 ylim([0 1]);
-label_legend();
+label_axis();
+
+% Worst-case coverage of Wald confidence ellipsoid
+figure('Units','inches','Position',plot_pos.*[1 1 1.5 1]);
+for i_k=1:n_k
+    subplot(1,n_k,i_k);
+    hold on;
+    for i_M=1:n_M
+        plot(se_ratio,wc_prob_wald(i_M,:,i_k),linestyles{i_M},'LineWidth',1);
+    end
+    hold off;
+    the_xlim = xlim;
+    line(the_xlim,[1 1]*(1-signif),'Color','k','LineStyle','-'); % Mark nominal coverage level on vertical axis
+    xlim(the_xlim);
+    ylim([0 1]);
+    xlabel('$\sqrt{\lambda_{\mathrm{min}}(\mathrm{aVar}(\hat{\mbox{\boldmath$\delta$}})\mathrm{aVar}(\hat{\mbox{\boldmath$\beta$}})^{-1})}$','Interpreter','Latex');
+    set(gca,'FontSize',12);
+    title(sprintf('%s%d%s','$k=',k(i_k),'$'),'Interpreter','Latex','FontSize',14);
+    if i_k==n_k
+        label_legend(M);
+    end
+end
 
 
-%% Plotting function
+%% Plotting functions
 
-function label_legend(varargin)
+function label_axis()
     xlabel('$\sqrt{\mathrm{aVar}(\hat{\delta}_h)/\mathrm{aVar}(\hat{\beta}_h)}$','Interpreter','Latex');
     set(gca,'FontSize',12);
-    if ~isempty(varargin)
-        legend(strcat('M=', arrayfun(@num2str, varargin{1}, 'UniformOutput', 0)),'Location','SouthEast','FontSize',12);
-    end
+end
+
+function label_legend(M)
+    legend(strcat('M=', arrayfun(@num2str, M, 'UniformOutput', 0)),'Location','SouthEast','FontSize',12);
 end
