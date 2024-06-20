3696ffe5a9f8301eb45cb4a8c338f04a664bb2c7
mikkelpm <mikkel.p.m@gmail.com>
2024-06-20 10:35:04 -0400

    New confidence ellipsoid plot

--- analytics/figures_worstcase.m
+++ analytics/figures_worstcase.m
@@ -9,11 +9,13 @@ clear all;
 
 signif = 0.1;                           % Significance level
 se_ratio = sort([linspace(0.001,0.999,100) 0.25 0.5]); % Values of SE ratio to plot (sqrt{aVar(AR)/aVar(LP)})
-k = [2 5 10];                           % Values of k to plot (for joint Wald confidence ellipsoid)
+k = [1 2 5 10 20];                      % Values of k to plot (for joint Wald confidence ellipsoid)
 M = [0.1 1 1.5 2 3];                    % Values of M to plot
 linestyles = {'-k','--k','-.k',':k','.k'}; % Line style for each value of M
 plot_pos = [0 0 8 4];                   % Figure position (in inches)
 plot_ylim_length = [0 2];               % Vertical axis limits for relative length plot
+plot_ellipse_k = [2 3 4];               % Which indices of the k vector to use as subplots when plotting worst-case ellipsoid coverage as a function of M
+plot_ellipse_M = [2 3 4];               % Which indices of the M vector to use as subplots when plotting worst-case ellipsoid coverage as a function of k
 
 
 %% Compute
@@ -85,8 +87,8 @@ the_xlim = xlim;
 line(the_xlim,[1 1]*(1-signif),'Color','k','LineStyle','-'); % Mark nominal coverage level on vertical axis
 xlim(the_xlim);
 ylim([0 1]);
-label_axis();
-label_legend(M);
+label_axis(true);
+label_legend(M,'M');
 
 % Relative length of bias-aware VAR CI
 figure('Units','inches','Position',plot_pos);
@@ -99,8 +101,8 @@ the_xlim = xlim;
 line(the_xlim,[1 1],'Color','k','LineStyle','-'); % Mark 1 on vertical axis
 xlim(the_xlim);
 ylim(plot_ylim_length);
-label_axis();
-label_legend(M);
+label_axis(true);
+label_legend(M,'M');
 
 % Weight on LP in optimal bias-aware CI
 figure('Units','inches','Position',plot_pos);
@@ -110,8 +112,8 @@ for i_M=1:n_M
 end
 hold off;
 ylim([0 1]);
-label_axis();
-label_legend(M);
+label_axis(true);
+label_legend(M,'M');
 
 % Relative length of optimal bias-aware CI
 figure('Units','inches','Position',plot_pos);
@@ -121,8 +123,8 @@ for i_M=1:n_M
 end
 hold off;
 ylim([0 1]);
-label_axis();
-label_legend(M);
+label_axis(true);
+label_legend(M,'M');
 
 % Worst-case joint probability
 figure('Units','inches','Position',plot_pos);
@@ -131,37 +133,61 @@ the_xlim = xlim;
 line(the_xlim,[1 1]*signif,'Color','k','LineStyle',':'); % Mark nominal significance level on vertical axis
 xlim(the_xlim);
 ylim([0 1]);
-label_axis();
+label_axis(true);
 
 % Worst-case coverage of Wald confidence ellipsoid
+n_plot_k = length(plot_ellipse_k);
 figure('Units','inches','Position',plot_pos.*[1 1 1.5 1]);
-for i_k=1:n_k
-    subplot(1,n_k,i_k);
+for i_k=1:n_plot_k
+    subplot(1,n_plot_k,i_k);
     hold on;
     for i_M=1:n_M
-        plot(se_ratio,wc_prob_wald(i_M,:,i_k),linestyles{i_M},'LineWidth',1);
+        plot(se_ratio,wc_prob_wald(i_M,:,plot_ellipse_k(i_k)),linestyles{i_M},'LineWidth',1);
     end
     hold off;
     the_xlim = xlim;
     line(the_xlim,[1 1]*(1-signif),'Color','k','LineStyle','-'); % Mark nominal coverage level on vertical axis
     xlim(the_xlim);
     ylim([0 1]);
-    xlabel('$\sqrt{\lambda_{\mathrm{min}}(\mathrm{aVar}(\hat{\mbox{\boldmath$\delta$}})\mathrm{aVar}(\hat{\mbox{\boldmath$\beta$}})^{-1})}$','Interpreter','Latex');
-    set(gca,'FontSize',12);
-    title(sprintf('%s%d%s','$k=',k(i_k),'$'),'Interpreter','Latex','FontSize',14);
-    if i_k==n_k
-        label_legend(M);
+    label_axis(false);
+    title(sprintf('%s%d%s','$k=',k(plot_ellipse_k(i_k)),'$'),'Interpreter','Latex','FontSize',14);
+    if i_k==n_plot_k
+        label_legend(M,'M');
+    end
+end
+
+figure('Units','inches','Position',plot_pos.*[1 1 1.5 1]);
+n_plot_M = length(plot_ellipse_M);
+for i_M=1:n_plot_M
+    subplot(1,n_plot_M,i_M);
+    hold on;
+    for i_k=1:n_k
+        plot(se_ratio,wc_prob_wald(plot_ellipse_M(i_M),:,i_k),linestyles{i_k},'LineWidth',1);
+    end
+    hold off;
+    the_xlim = xlim;
+    line(the_xlim,[1 1]*(1-signif),'Color','k','LineStyle','-'); % Mark nominal coverage level on vertical axis
+    xlim(the_xlim);
+    ylim([0 1]);
+    label_axis(false);
+    title(sprintf('%s%3.2g%s','$M=',M(plot_ellipse_M(i_M)),'$'),'Interpreter','Latex','FontSize',14);
+    if i_M==1
+        label_legend(k,'k');
     end
 end
 
 
 %% Plotting functions
 
-function label_axis()
-    xlabel('$\sqrt{\mathrm{aVar}(\hat{\delta}_h)/\mathrm{aVar}(\hat{\beta}_h)}$','Interpreter','Latex');
+function label_axis(univariate)
+    if univariate
+        xlabel('$\sqrt{\mathrm{aVar}(\hat{\delta}_h)/\mathrm{aVar}(\hat{\beta}_h)}$','Interpreter','Latex');
+    else
+        xlabel('$\sqrt{\lambda_{\mathrm{min}}(\mathrm{aVar}(\hat{\mbox{\boldmath$\delta$}})\mathrm{aVar}(\hat{\mbox{\boldmath$\beta$}})^{-1})}$','Interpreter','Latex');
+    end
     set(gca,'FontSize',12);
 end
 
-function label_legend(M)
-    legend(strcat('M=', arrayfun(@num2str, M, 'UniformOutput', 0)),'Location','SouthEast','FontSize',12);
+function label_legend(val,s)
+    legend(strcat(strcat(s,'='), arrayfun(@num2str, val, 'UniformOutput', 0)),'Location','SouthEast','FontSize',12);
 end
